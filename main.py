7870cc72edc2941ba5846fba1f61c984000f01a6
Workshop Public Cloud <cj-cloud-wave@workshops.aws>
2025-07-24 08:34:38 +0000

    add_sidebar_Travel_Attractions

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ def side_bar():
         selected_page = option_menu(
             menu_title="Main Menu", 
             # options=["Main_Home"],
-            options=["Main_Home", "Photo_Gallery"],
-            # options=["Main_Home", "Photo_Gallery", "Travel_Attractions"],
+            # options=["Main_Home", "Photo_Gallery"],
+            options=["Main_Home", "Photo_Gallery", "Travel_Attractions"],
             icons=['bi bi-house', 'bi bi-images', 'bi bi-airplane-engines'],
             menu_icon="cast", 
             default_index=0,
